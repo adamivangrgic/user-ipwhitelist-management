a645704b9c9da9e1b6ddd1ae952d77527e904bbd
adamivangrgic <grgic.ivan.adam@gmail.com>
2024-12-18 22:45:59 +0100

    initial commit

---
+++ Readme.md
@@ -0,0 +1,12 @@
+git clone https://github.com/adamivangrgic/user-ipwhitelist-management.git
+cd user-ipwhitelist-management
+docker build -t user-ipwhitelist-management .
+docker run --name user-ipwhitelist-management -dp 9201:5000 user-ipwhitelist-management
+
+enviroment variables
+ACCESS_PASS = secure_access_password
+PERSISTENT_STORAGE = /data by default
+
+ip list endpoint:   http://127.0.0.1:5000/return_ips?pass=<env:ACCESS_PASS> ; pass not required if not in env variables
+new ip endpoint:    http://127.0.0.1:5000/new_ip?ip=<newIP>&username=<user>&userpass=<password>
+new user endpoint:  http://127.0.0.1:5000/new_user?username=<user>&userpass=<password>
---
+++ main.py
@@ -0,0 +1,111 @@
+from flask import Flask, request, Response
+import requests
+import os, os.path, time
+import json
+
+app = Flask(__name__)
+
+access_pass = os.environ.get('ACCESS_PASS')
+persistent_storage = os.environ.get('PERSISTENT_STORAGE', 'data')
+
+data_file_path = os.path.join(persistent_storage, 'data.json')
+
+
+def open_data():
+    if not os.path.isfile(data_file_path):
+        write_data({'users': {}})
+
+    with open(data_file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return data
+
+def write_data(data):
+    with open(data_file_path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+@app.route('/return_ips')
+def return_ips():
+
+    ## auth
+
+    if access_pass:
+        submitted_pass = request.args.get('pass')
+        
+        if access_pass != submitted_pass:
+            return Response('Forbidden', status=403)
+
+    ##
+
+    data = open_data()
+    ips = []
+
+    for user_key in data['users'].keys():
+        user = data['users'][user_key]
+
+        if user['enabled']:
+            
+            for ip_obj in user['ips']:
+                ips.append(ip_obj['ip'])
+
+    ips_out = {'data': ips}
+
+    return Response(json.dumps(ips_out), mimetype='application/json')
+
+@app.route('/new_user')
+def new_user():
+
+    ## auth
+
+    if access_pass:
+        submitted_pass = request.args.get('pass')
+        
+        if access_pass != submitted_pass:
+            return Response('Forbidden', status=403)
+
+    ##
+
+    username = request.args.get('username')
+    password = request.args.get('userpass')
+
+    # 'username': {'userpass': <>, 'enabled': True, 'ips': []}
+
+    user_obj = {'userpass': password, 'enabled': True, 'ips': []}
+
+    data = open_data()
+    data['users'][username] = user_obj
+    write_data(data)
+
+    return Response('OK')
+
+@app.route('/new_ip')
+def new_ip():
+    new_ip = request.args.get('ip')
+
+    username = request.args.get('username')
+    password = request.args.get('userpass')
+
+    ## auth
+
+    try:
+        access_pass = open_data()['users'][username]['userpass']
+
+        if access_pass != password:
+            raise KeyError()
+    except:
+        return Response('Forbidden', status=403)
+
+    ##
+
+    # {'ip': <>, 'time': <>}
+
+    ip_obj = {'ip': new_ip, 'time': time.time()}
+
+    data = open_data()
+    data['users'][username]['ips'].append(ip_obj)
+    write_data(data)
+
+    return Response('OK')
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
